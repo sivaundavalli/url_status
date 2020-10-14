c46d092db66cf39ffd3514afd1bee49c9c13ca06
Siva Undavalli <sivaundavallis@gmail.com>
2020-10-14 10:15:28 -0700

    updated the flask app

--- metrics_flask.py
+++ metrics_flask.py
@@ -15,14 +15,14 @@ global result
 def calculate_metrics():
   urls = ['https://httpstat.us/200', 'https://httpstat.us/503']
   for i in urls:
-      r = requests.get(i,timeout=0.09)
+      r = requests.get(i,timeout=1)
       status_code = r.status_code
       resp_time = (round(r.elapsed.total_seconds(), 3) * 1000)
       if status_code == 200:
-       result1 = "<h2> sample_external_url_up{{url=\"{urls} \"}}""  =  1 <br> sample_external_url_response_ms{{url={urls}}}"  "= [{resp}]</h2>".format(urls=i,resp=resp_time)
+       result1 = "<h2> sample_external_url_up{{url=\"{urls} \"}}""  =  1 <br> sample_external_url_response_ms{{url=\"{urls}\"}}"  "= [{resp}]</h2>".format(urls=i,resp=resp_time)
       else:
-       result2 =("<h2> sample_external_url_up{{url = {urls}}}"  "= 0 <br>  sample_external_url_response_ms{{url={urls}}}"  "= [{resp}]</h2>".format(urls=i, resp=resp_time))
-  return  (result1+result2)
+       result2 = "<h2> sample_external_url_up{{url =\"{urls} \"}}"" =  0 <br> sample_external_url_response_ms{{url=\"{urls}\"}}"  "= [{resp}]</h2>".format(urls=i,resp=resp_time)
+  return  result1+result2
 
 if __name__ == '__main__':
     app.run(debug=True)
